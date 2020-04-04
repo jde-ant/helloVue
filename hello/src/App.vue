82d86bc672b612113245804e0bb1737bd9bcad40
Jorge de A <jorgedeantonio2@gmail.com>
2020-04-04 21:10:13 +0200

    changes

--- hello/src/App.vue
+++ hello/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/rat2.jpg">
+    <img alt="Personal photo" src="./assets/jor_small.png">
     <HelloWorld msg="Hello World"/>
-    <span>Jorge's personal page{{ msg }}</span>
+    <span>Work in progress...{{ msg }}</span>
   </div>
 </template>
 
